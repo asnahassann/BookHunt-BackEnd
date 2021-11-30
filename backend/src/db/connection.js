9ac12dff3ade3bc2f8dc2f12de41fe1b08af72f4
asnahassan <65673886+asnahassan@users.noreply.github.com>
2021-11-30 15:29:43 +0100

    added user.model fav

---
+++ backend/src/db/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Sucessfully connected");
+    } catch (error) {
+        console.log(error)    
+    }
+}
+
+connection();
---
+++ backend/src/user/user.routes.js
@@ -0,0 +1,15 @@
+const { Router } = require("express");
+const { addUser, logIn, listFavBook, unfavouriteBook, favouriteBook  } = require("./user.controllers");
+const { hashPassword, comparePasswords, tokenAuth } = require("../middleware/index");
+const userRouter = Router(); 
+
+userRouter.post("/user", hashPassword, addUser); 
+
+userRouter.post("/login", comparePasswords, logIn);
+
+userRouter.get("/token", tokenAuth, logIn);
+
+userRouter.put("/profile", tokenAuth, listFavBook);
+
+module.exports = userRouter; 
+
